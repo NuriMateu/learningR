e39772c1355b6d0fc2b4e4f214286b11dec1d4af
Nuri Cayuelas Mateu <nuricayuelas@hotmail.com>
2021-06-15 15:25:35 +0200

    old versions

---
+++ R/project-session.R
@@ -0,0 +1,12 @@
+10
+source(here:: here("R/package-loading.R"))
+
+# Basics of R -------------------------------------------------------------
+
+weight_kilos <-100
+c("a", "b", "c")
+factor(c("high", "medium", "low"))
+head(CO2)
+colnames(CO2)
+str(CO2)
+summary(CO2)
