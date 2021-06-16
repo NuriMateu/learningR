9de6aac019ac26d08defb3287d07a6c595285279
Nuri Cayuelas Mateu <nuricayuelas@hotmail.com>
2021-06-16 14:07:30 +0200

    mean median

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -139,3 +139,37 @@ nhanes_small %>%
 nhanes_small %>%
     summarise(max_bmi = max(bmi, na.rm = TRUE),
               min_bmi = min(bmi, na.rm = TRUE))
+
+# 1.
+nhanes_small %>%
+    summarise(mean_weight = mean(weight, na.rm = TRUE),
+              mean_age = mean(age, na.rm = TRUE)
+
+# 2.
+nhanes_small %>%
+    summarise(max_height = max(height, na.rm = TRUE),
+              min_height = min(height, na.rm = TRUE)
+
+# 3.
+nhanes_small %>%
+    summarise(median_age = median(age, na.rm = TRUE),
+              median_phys_active_days = median(phys_active_days, na.rm = TRUE))
+
+# calculate summary statistics by groups
+nhanes_small %>%
+    group_by(diabetes) %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE))
+
+
+nhanes_small %>%
+    # Recall ! means "NOT", so !is.na means "is not missing"
+    filter(!is.na(diabetes)) %>%
+    group_by(diabetes) %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE)) %>%
+ungroup()
+
+## save data sets as files
+#saving data as an .rda file in the data folder
+usethis::use_data(nhanes_small, overwrite = TRUE)
